dc47c2342580938308cea32f00547b622afcd6f3
kaouther <kaoutherboutheldja@gmail.com>
2023-11-18 21:58:59 +0100

    add inorderTraversal.c

---
+++ inorderTraversal.c
@@ -0,0 +1,10 @@
+#include "tree.h"
+
+// In-order traversal (Left-Root-Right)
+void inorderTraversal(struct TreeNode* root) {
+    if (root != NULL) {
+        inorderTraversal(root->left);
+        printf("%d ", root->data);
+        inorderTraversal(root->right);
+    }
+}
