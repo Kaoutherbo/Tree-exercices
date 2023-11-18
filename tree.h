46f4d17d656dacd308d04d09e8f37bb5b539bef9
kaouther <kaoutherboutheldja@gmail.com>
2023-11-18 21:46:25 +0100

    add tree.h

---
+++ tree.h
@@ -0,0 +1,21 @@
+#ifndef TREE_H
+#define TREE_H
+
+// Header files
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definition for a binary tree node
+typedef struct TreeNode {
+    int key;
+    struct TreeNode* left;
+    struct TreeNode* right;
+}TreeNode;
+
+// prototypes of functions
+TreeNode* createNode(int key);
+void inorderTraversal(struct TreeNode* root);
+void preorderTraversal(struct TreeNode* root);
+void postorderTraversal(struct TreeNode* root);
+
+#endif  /* TREE_H */
