698a8eaabc12e95262cd8d4b50ee874aa0db14e2
kaouther <kaoutherboutheldja@gmail.com>
2023-11-18 21:56:25 +0100

    add tree.c

---
+++ tree.c
@@ -0,0 +1,33 @@
+#include "tree.h"
+
+int main() {
+    // Example of a binary tree
+    //        1
+    //       / \
+    //      2   3
+    //     / \
+    //    4   5
+
+    struct TreeNode* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    // In-order traversal: 4 2 5 1 3
+    printf("In-order traversal: ");
+    inorderTraversal(root);
+    printf("\n");
+
+    // Pre-order traversal: 1 2 4 5 3
+    printf("Pre-order traversal: ");
+    preorderTraversal(root);
+    printf("\n");
+
+    // Post-order traversal: 4 5 2 3 1
+    printf("Post-order traversal: ");
+    postorderTraversal(root);
+    printf("\n");
+
+    return 0;
+}
