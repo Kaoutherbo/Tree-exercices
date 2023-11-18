66033ad54fbbbdcb1ebb8ca4d03d6b7e561132c4
kaouther <kaoutherboutheldja@gmail.com>
2023-11-18 21:59:59 +0100

    add preorderTraversal.c

---
+++ preorderTraversal.c
@@ -0,0 +1,10 @@
+#include "tree.h"
+
+// Pre-order traversal (Root-Left-Right)
+void preorderTraversal(struct TreeNode* root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preorderTraversal(root->left);
+        preorderTraversal(root->right);
+    }
+}
