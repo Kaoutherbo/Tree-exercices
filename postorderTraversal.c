5a29f5bef167dbb62fbb7d30b5603675aa1c7314
kaouther <kaoutherboutheldja@gmail.com>
2023-11-18 22:01:02 +0100

    add postorderTraversal.c

---
+++ postorderTraversal.c
@@ -0,0 +1,10 @@
+#include "tree.h"
+
+// Post-order traversal (Left-Right-Root)
+void postorderTraversal(struct TreeNode* root) {
+    if (root != NULL) {
+        postorderTraversal(root->left);
+        postorderTraversal(root->right);
+        printf("%d ", root->data);
+    }
+}
