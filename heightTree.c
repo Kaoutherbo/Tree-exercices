fcdffe71619d5aba51c8405a9d34c09692f2fca5
kaouther <kaoutherboutheldja@gmail.com>
2023-11-19 16:02:08 +0100

    add some changes

--- heightTree.c
+++ heightTree.c
@@ -1,11 +1,18 @@
 #include "tree.h"
 
+/**
+ * heightTree - fuction that return
+ * the height of the tree
+ * 
+ * @root: pointer to the root of tree
+ * Return: height of the tree
+*/
 int heightTree(TreeNode* root)
 {
     if(root == NULL) return 0;
-    int x = heightTree(root->left);
-    int y = heightTree(root->right);
 
-    if( x > y) return 1 + heightTree(root->left);
-    else return 1 + heightTree(root->right);
+    if( heightTree(root->left) > heightTree(root->right))
+       return 1 + heightTree(root->left);
+    else 
+       return 1 + heightTree(root->right);
 }
